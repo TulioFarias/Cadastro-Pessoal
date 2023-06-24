2de40b0006ed593e7a9c5dbb43994a40954e779e
TulioFarias <tuliott10@gmail.com>
2023-06-24 13:57:21 -0300

    projeto-cadastro-pessoal

---
+++ index.js
@@ -0,0 +1,14 @@
+const button = document.getElementById("cadastro")
+
+
+
+  function Alerta(){
+    alert('Seu cadastro foi realizado com sucesso!')
+    
+}
+
+button.addEventListener('Click', Alerta)
+
+
+
+
